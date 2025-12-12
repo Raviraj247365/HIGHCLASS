db9c7f1599ff757b84c6bcd174345689799f9c77
Raviraj247365 <ravirajmishra2004@gmail.com>
2025-12-12 18:15:00 +0530

    Updated CSS for product page layout

---
+++ backend/controllers/authController.js
@@ -0,0 +1,53 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// REGISTER
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json({ msg: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// LOGIN
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ msg: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid password" });
+
+    // JWT Token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      msg: "Login successful",
+      token,
+      user: { id: user._id, name: user.name, email: user.email }
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
